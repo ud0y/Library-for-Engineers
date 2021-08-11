f3d7eda4a8f173c9179a7e6c3af7de0f23d42ca6
Udoy Das <udoydhruba153@gmail.com>
2021-08-11 10:22:08 +0600

    First commit

---
+++ app/src/main/java/com/example/le/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.le;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+
+        }
+}
---
+++ app/src/main/java/com/example/le/SplashScreen.java
@@ -0,0 +1,44 @@
+package com.example.le;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent i = new Intent(SplashScreen.this,
+                    MainActivity.class);
+            startActivity(i);
+            finish();
+        }, 3700);
+
+        RotateAnimation rotate = new RotateAnimation(
+                0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        rotate.setDuration(1000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        ImageView imageView = findViewById(R.id.rotateImg);
+        imageView.startAnimation(rotate);
+    }
+}
